47d291373822cb39556b6725baabb394e827406c
Abhishek Mishra <abhishek@digiqt>
2022-10-11 00:45:16 +0530

    utils added

--- controllers/UploadController.go
+++ controllers/UploadController.go
@@ -1,13 +1,12 @@
 package controllers
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/golu360/go-file-server/schemas"
+	"github.com/golu360/go-file-server/utils"
 )
 
 func HandleFileUpload(c *gin.Context) {
@@ -33,25 +32,15 @@ func HandleFileUpload(c *gin.Context) {
 }
 
 func GetFS(c *gin.Context) {
-	var directories []string
-	fs, err := os.ReadDir("data")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	for _, f := range fs {
-		if f.Type().IsDir() {
-			directories = append(directories, f.Name())
-		}
-	}
+	var fileUtils utils.FileUtils = utils.FileUtils{DirName: "data"}
+
 	c.IndentedJSON(http.StatusOK, gin.H{
-		"keys": directories,
+		"keys": fileUtils.GetKeys(),
 	})
 }
 
 func CreateKey(c *gin.Context) {
 	var request schemas.CreateKeyRequest
-
 	if err := c.BindJSON(&request); err != nil {
 		log.Println(err)
 		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
@@ -59,35 +48,15 @@ func CreateKey(c *gin.Context) {
 		})
 		return
 	}
-	if _, err := os.Stat("data/" + request.KeyName); !os.IsNotExist(err) {
-		log.Println(err)
+	var fileUtils utils.FileUtils = utils.FileUtils{DirName: "data"}
+	var created bool = fileUtils.CreateKey(request.KeyName)
+	if !created {
 		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
-			"message": "Internal Server Error",
+			"message": "Could not create key",
 		})
 		return
-	}
-
-	err := os.Mkdir("data/"+request.KeyName, 0755)
-	if err != nil {
-		log.Println("Error Creating Key")
-		log.Println(err)
-		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
-			"message": "Internal Server Error",
-		})
-		return
-	}
-	var directories []string
-	fs, err := os.ReadDir("data")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	for _, f := range fs {
-		if f.Type().IsDir() {
-			directories = append(directories, f.Name())
-		}
 	}
 	c.IndentedJSON(http.StatusOK, gin.H{
-		"keys": directories,
+		"keys": fileUtils.GetKeys(),
 	})
 }
